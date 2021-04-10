3b8539a3a805ae299571697708e74c64ae50f6a1
ian <theplasmid2@gmail.com>
2021-04-11 03:37:09 +0800

    fixed bleu score issues

--- test.py
+++ test.py
@@ -5,16 +5,16 @@ Inference Loop for MNMT
 import torch
 import time
 from models import base_transformer
-from train import to_devices, accuracy_fn
 from models import initialiser
 from common import data_logger as logging
-from common.metrics import compute_bleu
+from common.metrics import BLEU
 from common import preprocess
 from common.test_arguments import test_parser
 from hyperparams.loader import Loader
+from common.utils import to_devices, accuracy_fn, mask_after_stop
 
 
-def inference_step(x, y, model, logger, tokenizer, device, teacher_forcing=False):
+def inference_step(x, y, model, logger, tokenizer, device, bleu, teacher_forcing=False):
     """
     inference step.
     x: source language
@@ -36,7 +36,7 @@ def inference_step(x, y, model, logger, tokenizer, device, teacher_forcing=False
             y_pred, _ = model(x, y_inp, enc_mask, look_ahead_mask, dec_mask)
 
         batch_acc = accuracy_fn(y_pred.detach(), y_tar).cpu().item()
-        batch_bleu = compute_bleu(y_tar, y_pred)
+        bleu(torch.argmax(y_pred, axis=-1), y_tar)
 
         logger.log_examples(y_tar, torch.argmax(y_pred, axis=-1), tokenizer)
 
@@ -69,11 +69,11 @@ def inference_step(x, y, model, logger, tokenizer, device, teacher_forcing=False
         y_pred = torch.cat(y_pred, dim=1)
         batch_acc = 0
         # batch_acc = accuracy_fn(y_pred, y_tar)
-        batch_bleu = compute_bleu(y_tar, y_pred)
+        bleu(y_pred, y_tar)
 
         logger.log_examples(y_tar, y_pred, tokenizer)
 
-    return batch_acc, batch_bleu
+    return batch_acc
 
 
 def test(device, params, test_dataloader, tokenizer):
@@ -86,27 +86,29 @@ def test(device, params, test_dataloader, tokenizer):
     model = initialiser.initialise_model(train_params, device)
     model = logging.load_checkpoint(logger.checkpoint_path, device, model)
 
-    test_batch_accs, test_batch_bleus = [], []
+    test_batch_accs = []
+    bleu = BLEU()
+    bleu.set_excluded_indices([0, 2])
 
     test_acc = 0.0
-    test_bleu = 0.0
     start_ = time.time()
 
     print("Now testing")
     for i, (x, y) in enumerate(test_dataloader):
 
-        test_batch_acc, test_batch_bleu = inference_step(x, y, model, logger, tokenizer, device, params.teacher_forcing)
+        test_batch_acc = inference_step(x, y, model, logger, tokenizer, device, bleu,
+                                                         params.teacher_forcing)
 
         test_batch_accs.append(test_batch_acc)
-        test_batch_bleus.append(test_batch_bleu)
 
         test_acc += (test_batch_acc - test_acc) / (i + 1)
-        test_bleu += (test_batch_bleu - test_bleu) / (i + 1)
+        curr_bleu = bleu.get_metric()
 
         if i % 50 == 0:
-            print('Accuracy {:.4f} Bleu {:.4f} in {:.4f} s per batch'.format(
-                i, test_acc, test_bleu, (time.time() - start_) / (i + 1)))
+            print('Epoch {} Accuracy {:.4f} Bleu {:.4f} in {:.4f} s per batch'.format(
+                i, test_acc, curr_bleu, (time.time() - start_) / (i + 1)))
 
+    test_bleu = bleu.get_metric()
     logger.log_results([params.langs, test_acc, test_bleu])
     logger.dump_examples()
 
@@ -133,4 +135,3 @@ if __name__ == "__main__":
     # Loader can also take in any dictionary of parameters
     params = Loader(args, check_custom=True)
     main(params)
-
