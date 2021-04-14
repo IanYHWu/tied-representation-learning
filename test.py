00bad1902fa9f06f84aa1a0dd66381a8d98eaf83
hamishs <hamish.scott@icloud.com>
2021-04-14 14:16:22 +0100

    added beam search (untestsed)

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ Inference Loop for MNMT
 """
 
 import torch
+import torch.nn.functional as F
 import time
 from tokenizers import Tokenizer
 from models import base_transformer
@@ -14,8 +15,62 @@ from common.test_arguments import test_parser
 from hyperparams.loader import Loader
 from common.utils import to_devices, accuracy_fn, mask_after_stop, get_all_directions
 
+def greedy_search(x, y, y_tar, model, enc_mask=None):
+    """Inference loop taking the most probable token at each step."""
+    x_enc = model.encoder(x, enc_mask)
+    y_pred = []  # List to store predictions made by model.
 
-def inference_step(x, y, model, logger, tokenizer, device, bleu, teacher_forcing=False):
+    # loop to compute output of decoder without teacher forcing
+    for t in range(y_tar.size(1)):
+        with torch.no_grad():
+            # Compute output of all translations up until sequence position t.
+            output = model.final_layer(model.decoder(y, x_enc, None, None)[0])
+            # Take most recently computed time step.
+            output = output[:, -1, :].squeeze()
+            # Retrieve predicted token.
+            max_token = torch.argmax(output, dim=-1).unsqueeze(-1)
+            y = torch.cat([y, max_token], dim=1)
+            y_pred.append(max_token)
+
+    return torch.cat(y_pred, dim=1)
+
+def beam_search(x, y, y_tar, model, enc_mask=None, beam_length=2):
+    """Inference loop exploring the n most probable paths at each step."""
+    x_enc = model.encoder(x, enc_mask)
+    decode = lambda y : F.log_softmax(model.final_layer(model.decoder(y, x_enc, None, None)[0]), dim=-1)
+
+    # initial beams and probabilities
+    y = y.unsqueeze(1).repeat(1, beam_length, 1)
+    log_p = torch.zeros(y.size()[0], beam_length).to(y.device) # (batch, 1)
+
+    for t in range(y_tar.size(1)):
+        with torch.no_grad():
+
+            # expand beams
+            output = decode(y.reshape(-1, y.size()[-1]))[:, -1, :] # (batch*beam, vocab_size)
+            new_log_p, new_tokens = torch.topk(output, beam_length, dim=-1) # (batch*beam, beam)
+
+            # get probability of each beam and trim beams
+            log_p = log_p.unsqueeze(-1) + new_log_p.reshape(-1, beam_length, beam_length)
+            log_p, indices = torch.topk(log_p.reshape(-1, beam_length * beam_length), beam_length, dim=-1)
+
+            # get the new tokens for each beam (batch, beam)
+            new_tokens = torch.gather(new_tokens.reshape(-1, beam_length*beam_length), 1, indices)
+
+            # get the new beams
+            y_ = y.unsqueeze(2).repeat(1, 1, beam_length, 1).reshape(-1, beam_length*beam_length, y.size()[-1])
+            new_y = torch.gather(y_, 1, indices.unsqueeze(-1).repeat(1, 1, y_.size()[-1])) # (batch, beam, seq_len)
+
+            y = torch.cat([new_y, new_tokens.unsqueeze(-1)], dim=-1) # (batch, beam, seq_len + 1)
+
+    # get the beam with the highest log prob
+    best_beam = log_p.argmax(-1, keepdim=True) # (batch,)
+    y = torch.gather(y, 1, best_beam.unsqueeze(-1).repeat(1, 1, y_pred.size()[-1])) # (batch, tar_len+1)
+
+    return y[:, 0, 1:] # (batch, tar_len)
+
+
+def inference_step(x, y, model, logger, tokenizer, device, bleu, teacher_forcing=False, beam_length=1):
     """
     inference step.
     x: source language
@@ -31,7 +86,7 @@ def inference_step(x, y, model, logger, tokenizer, device, bleu, teacher_forcing
             (x, y_inp, y_tar, enc_mask, look_ahead_mask, dec_mask),
             device)
 
-        # forward
+        # inference
         model.eval()
         with torch.no_grad():
             y_pred, _ = model(x, y_inp, enc_mask, look_ahead_mask, dec_mask)
@@ -49,25 +104,14 @@ def inference_step(x, y, model, logger, tokenizer, device, bleu, teacher_forcing
         # devices
         x, y_tar, enc_mask = to_devices((x, y_tar, enc_mask), device)
 
-        # forward
+        # inference
         model.eval()
-        x_enc = model.encoder(x, enc_mask)
-        y_pred = []  # List to store predictions made by model.
-
-        # loop to compute output of decoder without teacher forcing
-        for t in range(y_tar.size(1)):
-            with torch.no_grad():
-                # Compute output of all translations up until sequence position t.
-                output = model.final_layer(model.decoder(y, x_enc, None, None)[0])
-                # Take most recently computed time step.
-                output = output[:, -1, :].squeeze()
-                # Retrieve predicted token.
-                max_token = torch.argmax(output, dim=-1).unsqueeze(-1)
-                y = torch.cat([y, max_token], dim=1)
-                y_pred.append(max_token)
+        if beam_length == 1:
+            y_pred = greedy_search(x, y, y_tar, model, enc_mask=enc_mask)
+        else:
+            y_pred = beam_search(x, y, y_tar, model, enc_mask=enc_mask, beam_length=beam_length)
 
         # loss and metrics
-        y_pred = torch.cat(y_pred, dim=1)
         batch_acc = 0
         # batch_acc = accuracy_fn(y_pred, y_tar)
         bleu(y_pred, y_tar)
